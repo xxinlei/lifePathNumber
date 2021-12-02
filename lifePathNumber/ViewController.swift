43ff30848dd4c1549b657525376f824bcf11acbf
xinlei <xxinlei13@gmail.com>
2021-12-02 15:17:47 +0800

    Initial Commit

---
+++ lifePathNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifePathNumber
+//
+//  Created by 楊昕蕾 on 2021/12/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
